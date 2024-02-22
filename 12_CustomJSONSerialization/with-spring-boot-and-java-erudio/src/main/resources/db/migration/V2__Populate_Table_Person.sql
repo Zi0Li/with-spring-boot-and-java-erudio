c09eac99710b004756db6e48d4d6278cc066930a
Marcelo Zioli <103050927+Zi0Li@users.noreply.github.com>
2024-02-22 16:58:35 -0300

    Custom JSON Serialization

---
+++ 12_CustomJSONSerialization/with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Ppta', 'Marceloooo', 'Male', 'Zioli'),
+	(2, 'Ppta', 'Victor', 'Female', 'Hugo'),
+	(4, 'Assis', 'Thaynara', 'Female', 'Erreiro'),
+	(7, 'SP', 'Ronaldo', 'Male', 'Gaucho'),
+	(8, 'Assis', 'Testeee', 'Female', 'Erreiro');
